e379cf988ddf4c7d939df23b5a22a75554b666a7
Jana <s2022BockovaJana@skolabaltaci.cz>
2024-08-07 21:13:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
 })
 export class AppComponent {
-  title = 'Todo_App';
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems: any[] = [];
+
+  constructor() {
+    this.loadItems();
+  }
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+    this.saveItems();
+  }
+
+  deleteItem(item: any) {
+    this.allItems = this.allItems.filter((i) => i !== item);
+    this.saveItems();
+  }
+
+  loadItems() {
+    const storedItems = localStorage.getItem('todo-items');
+    if (storedItems) {
+      this.allItems = JSON.parse(storedItems);
+    }
+  }
+
+  saveItems() {
+    localStorage.setItem('todo-items', JSON.stringify(this.allItems));
+  }
 }
